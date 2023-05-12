691be20037aab595bcf411af8fcdf9aa51351059
Arilson1 <arilsontere18@hotmail.com>
2023-05-12 11:28:24 -0300

    first commit

---
+++ src/main/java/com/aatorganicos/aatorganicos/AatOrganicosApplication.java
@@ -0,0 +1,13 @@
+package com.aatorganicos.aatorganicos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AatOrganicosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AatOrganicosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aatorganicos/aatorganicos/AatOrganicosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aatorganicos.aatorganicos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AatOrganicosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
