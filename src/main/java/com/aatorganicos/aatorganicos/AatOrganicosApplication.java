d4f7f99af43996eb13aa7ccf861d762684bde532
Arilson1 <arilsontere18@hotmail.com>
2023-05-18 20:30:52 -0300

    Ajuste conexao banco h2

--- src/main/java/com/aatorganicos/aatorganicos/AatOrganicosApplication.java
+++ src/main/java/com/aatorganicos/aatorganicos/AatOrganicosApplication.java
@@ -1,7 +1,12 @@
 package com.aatorganicos.aatorganicos;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.aatorganicos.aatorganicos.model.Categoria;
+import com.aatorganicos.aatorganicos.repository.ICategoriaRepository;
 
 @SpringBootApplication
 public class AatOrganicosApplication {
@@ -9,5 +14,19 @@ public class AatOrganicosApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AatOrganicosApplication.class, args);
 	}
+
+	@Bean
+	CommandLineRunner initDatabase(ICategoriaRepository categoriaRepository) {
+		return args -> {
+			categoriaRepository.deleteAll();
+
+			Categoria c = new Categoria();
+			
+			c.setNome("Teste");
+			c.setDescricao("Teste descrição");
+
+			categoriaRepository.save(c);
+		};
+	};
 
 }
